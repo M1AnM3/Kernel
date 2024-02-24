40e26f0eb0ec003b54ad9188e9803d733e55eb56
M1AnM3 <129623786+M1AnM3@users.noreply.github.com>
2024-02-24 01:45:43 -0600

    Update kf.py

--- kf.py
+++ kf.py
@@ -52,10 +52,8 @@ def max_of_G(G):
     m = {G.in_degree(v) for v in G.nodes}
     return (max(m),m)
 
-# Información básica de la digráfica
-print(f'Palabras:{Aa}\nMaximo in-grado de la digráfica:{max_of_G(G)}\n Cantidad de palabras: {len(G.nodes)}\n Cantidad de oraciones: {len(A)}\n  Grado de entrada: {G.in_degree}\n Grado de salida: {G.out_degree}\n Grado: {G.degree}\n Diametro de la gráfica subyacente: {nx.diameter(G.to_undirected())}\n Excentricidad: {dict(nx.eccentricity(G.to_undirected()))}\n Centralidad de los vertices: {nx.degree_centrality(G)}')
-
 # Función para checar que un conjunto S tiene la propiedad de ser 2 absorbente, para que cambiar a 1 absorbente cambiar el 2 por 1
+# OJO: Si se selecciona que sea 1 aborbente, el programa puede estancarse en un while loop
 def dist_2(G, S):
     for node in set(G.nodes) - S:
         if all(nx.shortest_path_length(G, node, s) > 2 for s in S if nx.has_path(G, node, s)):
@@ -63,46 +61,70 @@ def dist_2(G, S):
     return True
 
 # Función para encontrar conjunto independiente    
-def Independent_set(G):
-    ind = set()
-    Nodes = set(G.nodes)
+def find_independent_set(graph):
+    fis = set()
+    nodes = list(graph.nodes())
 
-    while Nodes:
-        v = random.choice(list(Nodes))
-        if v in Nodes:
-            Nodes.remove(v)
+    Po = [node for node in nodes if G.out_degree(node)==0]
 
-        predecessors = set(G.predecessors(v))
-        Nodes -= predecessors
+    fis.update(Po)
 
-        for u in predecessors:
-            Nodes -= set(G.predecessors(u))
+    for v in Po:
+        prePo = G.predecessors(v)
+        nodes -= list(prePo)
 
-        if all(G.has_edge(v, u) == False for u in ind):
-            ind.add(v)
-        if not all(G.has_edge(u, v) == False for u in ind):
-            ind.remove(v)
-    
+    while nodes:
+        node = nodes.pop()
+        fis.add(node)
+        nodes = [n for n in nodes if not (n in graph[node] or node in graph[n])]
+
     # Para ver el proceso de encontrar el conjunto independiente descomentar la siguiente línea
     #print(f'Nodos: {Nodes}\nQuasi-kernel: {ind}')
-
-    return ind
+    
+    return fis
 
 # Función para checar que el conjunto dado por la función anterior satisface 
 # ser 2-absorbente
 def refine_set(G):
-    S = Independent_set(G)
+    S = find_independent_set(G)
     while not dist_2(G, S):
-        S = Independent_set(G)
+        S = find_independent_set(G)
     return S
 
 quasi_kernel_G = refine_set(G)
 
-print("Quasi-núcleo:", refine_set(G))
+# Info de la digráfica
+print(f'Palabras:{Aa}\n Cantidad de palabras: {len(G.nodes)}\n Cantidad de oraciones: {len(A)}\n Grado de entrada: {G.in_degree}\n Grado de salida: {G.out_degree}\n Grado: {G.degree}\n Diametro de la gráfica subyacente: {nx.diameter(G.to_undirected())}\n Excentricidad: {dict(nx.eccentricity(G.to_undirected()))}\n Centralidad de los vertices: {nx.degree_centrality(G)}\n (Quasi) Núcleo: {Narb}\n Cardinalidad del (quasi) núcleo: {len(Narb)}\n No (quasi) núcleo: {set(G.nodes - Narb)}\n Nodos1: {N1}\n Nodos2: {N2}')
+
+# Conjuntos auxiliares para una mejor re´resentación de la digráfica
+N2 = set(v for v in set(G.nodes) if any(nx.shortest_path_length(G, v, s) == 2 for s in Narb if nx.has_path(G, v, s)))
+
+N1 = set(v for v in set(G.nodes) if any(nx.shortest_path_length(G, v, s) == 1 for s in Narb if nx.has_path(G, v, s)))
+
+# Función para acomodar los nodos en círculos
+def calculate_positions(G, Narb, N1, N2):
+    positions = {}
+    num_nodes = len(G.nodes)
+    narb_nodes = len(Narb)
+    n1_nodes = len(N1)
+    n2_nodes = len(N2)
+    for i, node in enumerate(G.nodes):
+        if node in Narb:
+            theta = (num_nodes / (2 * math.pi)) + (i / (2 * math.pi))
+            positions[node] = (3 * math.cos(theta), 3 * math.sin(theta))
+        elif node in N1:
+            theta = (num_nodes / (2 * math.pi)) + (i / (2 * math.pi))
+            positions[node] = (2 * math.cos(theta), 2 * math.sin(theta))
+        elif node in N2:
+            theta = (num_nodes / (2 * math.pi)) + (i / (2 * math.pi))
+            positions[node] = (math.cos(theta), math.sin(theta))
+    return positions
+
+# Posición de los nodos segun la función anterior
+pos = calculate_positions(G, quasi_kernel_G, N1, N2)
 
 # Dibujar la gráfica con matplotlib
 node_colors = ["skyblue" if node in quasi_kernel_G else "red" for node in G.nodes]
-pos = nx.spring_layout(G)
 nodes = nx.draw_networkx_nodes(G, pos, node_color=node_colors, node_size=80)
 nx.draw_networkx_edges(G, pos, arrowsize=20)
 #nx.draw_networkx_labels(G, pos)
